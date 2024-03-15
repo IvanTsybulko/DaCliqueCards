c389cd0cceece549b59aed4dfd40d48e9eab0c79
IvanTsybulko <107126601+IvanTsybulko@users.noreply.github.com>
2024-03-15 11:39:35 +0200

    folders update

--- DaCliqueCardsApp/Classes/CurrClass.cs
+++ DaCliqueCardsApp/Classes/CurrClass.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaCliqueCardsApp;
 
 namespace DaCliqueCardsApp
 {
-    public static class CurrentCoach
+    public static class CurrClass
     {
-        public static Coach CurrCoach;
+        public static Class CurrentClass;
     }
 }
--- DaCliqueCardsApp/Classes/CurrentCoach.cs
+++ DaCliqueCardsApp/Classes/CurrentCoach.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaCliqueCardsApp;
 
 namespace DaCliqueCardsApp
 {
-    public static class CurrClass
+    public static class CurrentCoach
     {
-        public static Class CurrentClass;
+        public static Coach CurrCoach;
     }
 }
